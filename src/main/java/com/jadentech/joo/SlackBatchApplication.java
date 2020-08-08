2873d259209180b3c048277adabb1b9dbdc2726c
joo-pe <joohyung1002@naver.com>
2020-08-08 10:32:16 +0900

    init commit

--- src/main/java/com/jadentech/joo/SlackBatchApplication.java
+++ src/main/java/com/jadentech/joo/SlackBatchApplication.java
@@ -4,6 +4,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * jaden create
+ */
 @SpringBootApplication
 @EnableBatchProcessing
 public class SlackBatchApplication {
